e4bce29d06d0a4aa2497a985fa1be547af38b9b1
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2024-12-03 12:41:53 +0100

    added get for project and project content

--- Service Project/DTOs/ProjectContentResponseDto.cs
+++ Service Project/DTOs/ProjectContentResponseDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace Service_Project.DTOs;
 
-public class ProjectResponseDto
+public class ProjectContentResponseDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public object Dictionary { get; set; }  // The file structure from the blob storage
 }
--- Service Project/Repositories/IBlobRepository.cs
+++ Service Project/Repositories/IBlobRepository.cs
@@ -4,6 +4,7 @@ namespace Service_Project.Repositories;
 
 public interface IBlobRepository
 {
+    Task<Result<Dictionary<string, List<string>>>> GetContainerContent(Guid id);
     Task<Result> CreateContainerAsync(Guid id);
     Task<Result> DeleteContainerAsync(Guid id);
 }
