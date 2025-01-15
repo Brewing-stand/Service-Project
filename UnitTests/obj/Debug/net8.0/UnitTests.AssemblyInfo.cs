d2e91419653b95e3a2f9aa0373f62f02c5620b17
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2025-01-15 12:19:05 +0100

    kubershitties

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c1976d4a7e85068ea71fb3d21313c5b35ae7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a82e749b52c5e0bf0f9f212086993ea7cd328ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
